b2f3d1528eee299de5cbf5cffcdbaa24e9fb1364
pallavee-2705 <pallaveebisoyee@gmail.com>
2024-04-04 14:09:04 -0400

    added features to summary

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { items_bg, sun, hotairballoon, add, minus } from '../assets';
+import { items_bg, sun, hotairballoon, add, minus, progress_bar_items } from '../assets';
 import Cloud from '../components/Cloud';
 import '../styles/truckAnimation.css';
 import MovingTruck from '../components/MovingTruck';
@@ -104,7 +104,7 @@ const Items = () => {
         ))}
       </div>
 
-      <MovingTruck link="/baseline" />
+      <MovingTruck link="/boxes" />
       </div>
       <div className='w-full mt-20 -z-[30] flex justify-end'>
         <img src={items_bg} alt="items_bg" className="w-full"/>
--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -3,26 +3,26 @@ import { boxes_img } from '../assets';
 
 const Summary = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-full pt-12">
-      <div className="flex gap-6 text-neutral-50 text-xl">
-        <div className="w-auto h-auto bg-gray-400 p-5 rounded-xl">Tips for Saving Costs</div>
-        <div className="w-auto h-auto bg-gray-400 p-5 rounded-xl">Cost Breakdown</div>
-        <div className="w-auto h-auto bg-orange-400 p-5 rounded-xl">Order Summary</div>
+    <div className="relative flex flex-col justify-center items-center h-full pt-12">
+      <div className="flex gap-6 text-neutral-50 text-xl text-center">
+        <div className="w-40 h-auto bg-gray-500 p-3 rounded-xl">המלצות   לחיסכון בעלויות</div>
+        <div className="w-40 h-auto bg-gray-500 p-5 rounded-xl">פירוט עלויות</div>
+        <div className="w-auto h-auto bg-orange-400 p-5 rounded-xl">סיכום הזמנה</div>
       </div>
 
-     {/* Red circle with outer shadow */}
-     <div className=" absolute top-8 right-20 w-24 h-24 bg-red-500 rounded-full shadow-2xl p-4 text-white text-xl">Call for Bids</div>
+      {/* Red circle with outer shadow */}
+      <div className="absolute top-8 right-20 w-24 h-24 bg-red-500 rounded-full shadow-2xl p-5 text-white text-2xl"> צור מכרז</div>
 
-     <div className='pt-12 text-2xl'> Total Moving Costs </div>
-     <div className='pt-6 text-7xl font-semibold'> $650 </div>
+      <div className='pt-12 text-2xl'> Total Moving Costs </div>
+      <div className='pt-6 text-7xl font-semibold'> $650 </div>
 
-     {/* Dotted grey box */}
-     <div className="border-dotted border border-gray-400 mt-6 p-5 h-auto w-full max-w-md">
-       The displayed price is the maximum price for this move. You can reduce the moving costs by 20% - 30% by creating a tender among moving companies. When opening a tender, moving companies receive your moving details and offer competitive prices. You will receive the prices directly to WhatsApp and can choose the best offer for you. Only when you agree to receive a call, your details will be transferred to the moving company.
-     </div>
+      {/* Dotted grey box */}
+      <div className="border-dashed border border-gray-500 mt-6 p-4 max-w-2xl" style={{ borderWidth: '2px', borderStyle: 'dashed', borderRadius: '8px' }}> {/* Adjusted border style and width */}
+          The displayed price is the maximum price for this move. You can reduce the moving costs by 20% - 30% by creating a tender among moving companies. When opening a tender, moving companies receive your moving details and offer competitive prices. You will receive the prices directly to WhatsApp and can choose the best offer for you. Only when you agree to receive a call, your details will be transferred to the moving company.
+      </div>
 
       {/* Background image */}
-      <img src={boxes_img} alt="background" className='absolute bottom-0 w-full h-1/3 max-md:w-14 max-md:h-24 z-30' />
+      <img src={boxes_img} alt="background" className='h-1/4 bottom-0 w-full max-md:w-14 max-md:h-24' />
     </div>
   );
 };
