5ec88cf3c7a57fc2b5694bafa20852a3271b0a2d
Denys Opria <girardjoyce74@gmail.com>
2024-04-27 03:17:15 +0300

    Google distance api

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,34 +1,42 @@
-import axios from 'axios';
+import axios from "axios";
 
-export async function getDistance() {
-    try {
-        const baselineAddress = '956 Richmond St, London, ON N6A 3J5, Canada';
-        const destinationAddress = '534 Blackwater Pl, London, ON N5X 4J4, Canada';
-        const apiKey = 'AIzaSyAuIchE5mdfEw_S7oM8I5ZkpCcQyWOMg-Y';
-        const apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${baselineAddress}&destination=${destinationAddress}&key=${apiKey}`
-        
-        const response = await axios.get(apiUrl)
-        console.log({response})
-        return response.data.rows[0].elements;
-    } catch (error) {
-        console.error(error.message);
-        return null;
-    }
+export function encodeGoogleMapsURLComponent(str) {
+	return str.replace(/ /g, "+");
 }
 
+export async function getDistance() {
+	try {
+		const baselineAddress = "956 Richmond St, London, ON N6A 3J5, Canada";
+		const destinationAddress =
+			"534 Blackwater Pl, London, ON N5X 4J4, Canada";
+		const apiUrl = `http://localhost:3000/distance`;
+
+		const response = await axios.get(apiUrl, {
+			params: {
+				origins: baselineAddress,
+				destinations: destinationAddress,
+			},
+		});
+		console.log({ response });
+		return response.data.rows[0].elements;
+	} catch (error) {
+		console.error(error.message);
+		return null;
+	}
+}
 
 export const calculateCranePrice = (floorCount) => {
-    // Base price for up to the 5th floor
-    let basePrice = 400;
-    
-    // Price for each additional floor above the 5th floor
-    const additionalPricePerFloor = 50;
-    
-    // Calculate the total price based on the floor count
-    if (floorCount > 5) {
-        basePrice += (floorCount - 5) * additionalPricePerFloor;
-    }
-    
-    // Double the price if there are multiple points
-    return basePrice
+	// Base price for up to the 5th floor
+	let basePrice = 400;
+
+	// Price for each additional floor above the 5th floor
+	const additionalPricePerFloor = 50;
+
+	// Calculate the total price based on the floor count
+	if (floorCount > 5) {
+		basePrice += (floorCount - 5) * additionalPricePerFloor;
+	}
+
+	// Double the price if there are multiple points
+	return basePrice;
 };
